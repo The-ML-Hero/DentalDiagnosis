3ad7d869b8726545f7689bc0c163ccc3e26efeab
The-ML-Hero <67855775+The-ML-Hero@users.noreply.github.com>
2020-10-27 14:23:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import os
 import sys
 import streamlit as st
+import streamlit.components.v1 as stc
 import tooth_reconstruction
 import display_app as udisp
-import Pulp_Segmentation
+#import Pulp_Segmentation
+import root_seg
 import fracture_tooth_detection
 import flurosis_tooth_detection
-
 MENU = {
-    "Tooth Fracture Reconstruction" : tooth_reconstruction,
+    
     "Fracture Tooth Detection" : fracture_tooth_detection,
-    "Pulp Segmentation" : Pulp_Segmentation,
-    "Flurosis Detection" : flurosis_tooth_detection,		
+    "Root Anatomy Prediction" : root_seg,
+    "Flurosis Detection" : flurosis_tooth_detection,	
+    "Tooth Fracture Reconstruction" : tooth_reconstruction,	
 }
-
 st.sidebar.title("Choose A Use Case")
 menu_selection = st.sidebar.radio("Use Case", list(MENU.keys()))
 menu = MENU[menu_selection]
 
 with st.spinner(f"Loading {menu_selection} ..."):
       udisp.render_page(menu)
+
